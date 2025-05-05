a4afab9493eabe85c5ae86abcd88f2f1787764c8
Vass Paul <vass_paul95@yahoo.com>
2025-05-05 09:44:07 +0300

    added calculation feature

--- src/main/java/com/scoala_informala/final_project/final_project/controller/InformationController.java
+++ src/main/java/com/scoala_informala/final_project/final_project/controller/InformationController.java
@@ -1,6 +1,9 @@
 package com.scoala_informala.final_project.final_project.controller;
 
 import com.scoala_informala.final_project.final_project.model.CovidInfo;
+import com.scoala_informala.final_project.final_project.model.Doctor;
+import com.scoala_informala.final_project.final_project.model.Specialty;
+import com.scoala_informala.final_project.final_project.service.AppointmentService;
 import com.scoala_informala.final_project.final_project.service.CovidInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -14,19 +17,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InformationController {
 
     private final CovidInfoService covidInfoService;
+    private final AppointmentService appointmentService;
 
-    @GetMapping("/covid")
-    public String getCovidInfo(Model model) {
+    @GetMapping
+    public String getInfo(Model model) {
 
         CovidInfo covidInfo = covidInfoService.getCovidInfoForRomania();
+        Doctor mostBookedDoctor = appointmentService.getMostBookedDoctor();
+        Specialty mostBookedSpecialty = appointmentService.getMostBookedSpecialty();
 
         if (covidInfo != null) {
             model.addAttribute("totalCases", covidInfo.getTotalCases());
             model.addAttribute("lastUpdate", covidInfo.getLastUpdate());
+            model.addAttribute("mostBookedDoctor", mostBookedDoctor);
+            model.addAttribute("mostBookedSpecialty", mostBookedSpecialty);
         } else {
             model.addAttribute("errorMessage", "Failed to retrieve COVID information.");
         }
 
-        return "covidInfo.html";
+        return "info.html";
     }
 }
--- src/main/java/com/scoala_informala/final_project/final_project/service/AppointmentService.java
+++ src/main/java/com/scoala_informala/final_project/final_project/service/AppointmentService.java
@@ -3,13 +3,19 @@ package com.scoala_informala.final_project.final_project.service;
 import com.scoala_informala.final_project.final_project.model.Appointment;
 import com.scoala_informala.final_project.final_project.model.Doctor;
 import com.scoala_informala.final_project.final_project.model.Patient;
+import com.scoala_informala.final_project.final_project.model.Specialty;
 import com.scoala_informala.final_project.final_project.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 @Service
 @RequiredArgsConstructor
@@ -30,5 +36,27 @@ public class AppointmentService {
         return Stream.iterate(tomorrow, day -> day.plusDays(1))
                 .limit(14)
                 .toList();
+    }
+
+    public Doctor getMostBookedDoctor() {
+        final List<Appointment> appointments = appointmentRepository.findAll();
+        final Map<Doctor, Integer> doctorAppointmentCount = appointments.stream()
+                .collect(groupingBy(Appointment::getDoctor, collectingAndThen(counting(), Long::intValue)));
+
+        return doctorAppointmentCount.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
+
+    public Specialty getMostBookedSpecialty() {
+        final List<Appointment> appointments = appointmentRepository.findAll();
+        final Map<Specialty, Integer> specialtyAppointmentCount = appointments.stream()
+                .collect(groupingBy(appointment -> appointment.getDoctor().getSpecialty(), collectingAndThen(counting(), Long::intValue)));
+
+        return specialtyAppointmentCount.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 }
